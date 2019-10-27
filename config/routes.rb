2abb0ab8657adf64dd135e0580a198e8be4293d2
Jye Bussa <bussajye@gmail.com>
2019-10-28 00:51:48 +1000

    products controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :users 
+  get 'products/index'
+  get 'products/new'
+  get 'products/edit'
+  get 'products/create'
+  get 'products/destroy'
+  # resources :users 
+  get 'users', to: 'users#new'
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
